853f1f1744112592055b5f961b957db607ed40ea
grasmarcussanti129 <wemmelkiarie@hotmail.com>
2025-09-30 09:45:28 -0500

    Update server.js to include a 404 route handler

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// 404 route handler
+app.use((req, res) => {
+    res.status(404).send('404: Page Not Found');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
